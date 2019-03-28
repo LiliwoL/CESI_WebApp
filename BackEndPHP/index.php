aa89a8ed992d4097315869449aaf79bf745106fe
LiliwoL <github@liliwol.fr>
2019-03-28 13:18:18 +0100

    Update index.php

--- BackEndPHP/index.php
+++ BackEndPHP/index.php
@@ -1,22 +1,22 @@
 <?php
 
-# Connexion à la base de données
-include ('db.php');
-db_connect();
+    # Connexion à la base de données
+    include ('db.php');
+    db_connect();
 
 
-# Requête sur la liste de TOUS les produits en base
-$sth = $GLOBALS['db']->prepare("SELECT * FROM products LIMIT 10");
-$sth->execute();
+    # Requête sur la liste de TOUS les produits en base
+    $sth = $GLOBALS['db']->prepare("SELECT * FROM products LIMIT 30");
+    $sth->execute();
 
 
-# Récupération de TOUS les produits dans un tableau $resultset
-while ($result = $sth->fetch(PDO::FETCH_ASSOC)) 
-{
-    $resultset[] = $result;
-}
+    # Récupération de TOUS les produits dans un tableau $resultset
+    while ($result = $sth->fetch(PDO::FETCH_ASSOC)) 
+    {
+        $resultset[] = $result;
+    }
 
 
-# Affichage des résultats au format JSON
-header('Content-Type: application/json');
-echo json_encode($resultset);
+    # Affichage des résultats au format JSON
+    header('Content-Type: application/json');
+    echo json_encode($resultset);
